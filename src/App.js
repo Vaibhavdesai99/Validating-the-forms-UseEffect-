07d952aeccc926ec408105f867207f38d0f21cbd
Vaibhavdesai99 <Vaibhavdesai510@gmail.com>
2023-05-05 17:45:20 +0530

    Updated LoggedIn -Stored credentials  to localstorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //Here we checking if the user is already logged in then we give direct access for that we getItem from localstorage and check for condition ===1 , then we set to setLoggedIN(true) again
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  //Empty array means this will be executed by once only.
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    // here we are stored data to localstorage when page refresh the user visit same page as before not wanted to be logging again
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
